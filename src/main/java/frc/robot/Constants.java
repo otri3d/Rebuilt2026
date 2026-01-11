6ba8a6ac7a231321f736c96a0316e476f5380588
Connor Godden <connor.godden2006@gmail.com>
2026-01-11 15:44:01 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -18,27 +18,33 @@ public final class Constants {
     public static final int kShooterForwardPowerAxis = 3;
   }
 
+  // 4 sparks
   public static class DriveConstants {
     public static final int kLeftMotorPort1 = 1;
     public static final int kLeftMotorPort2 = 2;
     public static final int kRightMotorPort1 = 3;
     public static final int kRightMotorPort2 = 4;
- 
   }
 
+  // 2 talons
   public static class IntakeConstants {
     public static final int kLeadMotorPort = 5;
     public static final int kFollowMotorPort = 6;
     public static final double kMotorSpeed = 1.0;
   }
 
+
+  // 2 talons 1 spark
   public static class ShooterConstants{
     public static final int kLeftShooterConstant = 7;
     public static final int kRightShooterConstant= 8;
+    public static final int kFinalMotorPort = 9;
     public static final double kMinShooterInput = 0.5;
     public static final Double kShootingSpeed = 1.0;
   }
 
+
+  // air
   public static class ClimbConstants {// assuming dual action solenoids
     public static final int[] kSolenoidPins = {1,2,3,4};
                                   // {forward1, reverse1, forward2, reverse2}
