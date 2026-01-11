be06cc96d09d1b7bfad594c9c7f1a96070ae5ae0
GarfiedtheDestroyer <dave.paquette@ontariotechu.net>
2026-01-11 11:52:54 -0500

    Renamed files to follow naming covnention
    Renmed to climbsubsystem.java, updated imported constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,11 +5,15 @@
 package frc.robot;
 
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
  * globally (i.e. public static). Do not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
@@ -23,7 +27,7 @@ public final class Constants {
     public static final int kLeftMotorPort2 = 2;
     public static final int kRightMotorPort1 = 3;
     public static final int kRightMotorPort2 = 4;
- 
+
   }
 
   public static class IntakeConstants {
@@ -31,10 +35,16 @@ public final class Constants {
     public static final double kMotorSpeed = 1.0;
   }
 
-  public static class ShooterConstants{
+  public static class ShooterConstants {
     public static final int kLeftShooterConstant = 3;
-    public static final int kRightShooterConstant= 4;
+    public static final int kRightShooterConstant = 4;
     public static final double kMinShooterInput = 0.5;
     public static final Double kShootingSpeed = 1.0;
+  }
+
+  public static class ClimbConstants {
+    // assuming dual action solenoids
+    public static final int[] kSolenoidPins = { 1, 2, 3, 4 };
+    // {forward1, reverse1, forward2, reverse2}
   }
 }
